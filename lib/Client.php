c25d09793a52a3b8b8a0221364801bce0e8fc5fe
aalopatin <aalopatin@yamoney.ru>
2018-08-24 17:41:51 +0300

    Класс Status отмечен deprecated

--- lib/Client.php
+++ lib/Client.php
@@ -86,7 +86,7 @@ class Client
     /**
      * Текущая версия библиотеки
      */
-    const SDK_VERSION = '1.0.16';
+    const SDK_VERSION = '1.0.17';
 
     /**
      * Имя HTTP заголовка, используемого для передачи idempotence key
